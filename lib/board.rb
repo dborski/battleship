23f992bf175173903d461537794e7c13fe9de577
Jack Cullen <jpcullen20@gmail.com>
2020-03-24 14:51:57 -0600

    Adds cells method and test that it returns 16 instances of the cell class

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,15 @@
 class Board
 
+  attr_reader :cells_hash
+  def initialize
+    @cells_hash = Hash.new
+    @coordinates = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4",]
+  end
+
+  def cells
+    @coordinates.each do |coordinate|
+      cells_hash[coordinate] = Cell.new(coordinate)
+    end
+    cells_hash
+  end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/board'
+require './lib/cell'
 
 class BoardTest < Minitest::Test
 
@@ -10,6 +11,19 @@ class BoardTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Board, @board
+  end
+
+  def test_cells_returns_a_hash
+    assert_instance_of Hash, @board.cells
+  end
+
+  def test_16_elements_in_hash
+    assert_equal 16, @board.cells.length
+  end
+
+  def test_keys_point_to_instances_of_cells
+    assert_instance_of Cell, @board.cells.values.first
+    assert_instance_of Cell, @board.cells.values.last
   end
 
 end
