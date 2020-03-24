78fc2049da607306a5497068ac0e9169cd37a0c9
Jack Cullen <jpcullen20@gmail.com>
2020-03-24 14:29:15 -0600

    Adds board class and test it exists

---
+++ test/board_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+
+  def setup
+    @board = Board.new
+  end
+
+  def test_it_exists
+    assert_instance_of Board, @board
+  end
+
+end
