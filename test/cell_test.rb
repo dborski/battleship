c9a049f70a64f1c1adf55b62394422a9e78aa476
Derek Borski <derek.borski@gmail.com>
2020-03-24 11:34:55 -0600

    Adds place_ship method and tests if place_ship changes empty to false and ship to have the ship

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -6,6 +6,7 @@ require './lib/cell'
 class CellTest < Minitest::Test
   def setup
     @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   def test_it_exists
@@ -20,8 +21,19 @@ class CellTest < Minitest::Test
     assert_equal nil, @cell.ship
   end
 
+  def test_cell_is_empty_by_default
+    assert_equal true, @cell.empty?
+  end
 
+  def test_place_ship_adds_to_ship_method
+    @cell.place_ship(@cruiser)
+    assert_equal @cruiser, @cell.ship
+  end
 
+  def test_place_ship_makes_cell_not_empty
+    @cell.place_ship(@cruiser)
+    assert_equal false, @cell.empty?
+  end
 
 
 end
