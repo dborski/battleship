99b0e976a922e5f6745904c62a61fd1590f523dc
Jack Cullen <jpcullen20@gmail.com>
2020-03-23 21:25:02 -0600

    Add initialize method and tested attributes

---
+++ test/ship_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_exists
+    assert_instance_of Ship, @cruiser
+  end
+
+  def test_has_readable_attributes
+    assert_equal "Cruiser", @cruiser.name
+    assert_equal 3, @cruiser.length
+  end
+end
