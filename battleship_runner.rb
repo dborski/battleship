610e74926a1b31a3331bf359ac081eed6c2d1d98
Jack Cullen <jpcullen20@gmail.com>
2020-03-31 20:16:51 -0600

    Remove boards on runner file

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -4,8 +4,5 @@ require './lib/ship'
 require './lib/turn'
 require './lib/game'
 
-user_board = Board.new
-computer_board = Board.new
-
-game = Game.new(user_board, computer_board)
+game = Game.new
 game.start
